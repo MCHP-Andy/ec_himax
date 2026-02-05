9f41ed5f173df55d096d46fc2c0bc3fe411a0074
MCHP-Andy <andy.chang@microchip.com>
2026-02-05 16:17:26 +0800

    feat(pwm): implement fan control

--- app/service/himax/service.c
+++ app/service/himax/service.c
@@ -2,7 +2,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/i3c.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
 
 #include <interface/himax.h>
@@ -10,6 +10,7 @@
 LOG_MODULE_REGISTER(himax, LOG_LEVEL_INF);
 
 static void service(void) {
+    int ret = 0;
     himax_ver_t ver;
 
     k_sleep(K_MSEC(2000));
@@ -23,9 +24,25 @@ static void service(void) {
     // himax_set_frame_rate(5);
 
     while (1) {
-        k_sleep(K_MSEC(1000));
+        int dis = 0;
 
-        // himax_get_user_distance(0);
+        k_sleep(K_MSEC(2000));
+
+        dis = himax_get_user_distance(0);
+        if (dis >= 0) {
+            LOG_INF("User 0 Distance Category: %d", dis);
+        } else {
+            LOG_ERR("Failed to get user distance");
+            continue;
+        }
+
+        uint8_t duty = dis*30; // Simple mapping: 0->0%, 1->30%, 2->60%, 3->90%
+        ret = app_fan_set(0, duty);
+        if (ret != 0) {
+            LOG_ERR("Failed to set fan duty cycle");
+        } else {
+            LOG_INF("Fan duty cycle set to %d%% based on distance category %d", duty, dis);
+        }
     }
 }
 
